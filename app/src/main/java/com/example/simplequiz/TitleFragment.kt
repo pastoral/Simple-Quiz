5547deef88f1beb6d25431b6111d7c97cc589d86
pastoral <munir.hoque@gmail.com>
2019-12-23 23:42:02 -0800

    Add Navigation and NavHostFragment

--- app/src/main/java/com/example/simplequiz/TitleFragment.kt
+++ app/src/main/java/com/example/simplequiz/TitleFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.simplequiz.databinding.FragmentTitleBinding
 
 /**
@@ -21,6 +22,9 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        binding.btnPlay.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_quizFragment3)
+        }
         return binding.root
     }
 
