0ce82e486b1532f1c1d00c86bb856c2a1c902ca0
pastoral <munir.hoque@gmail.com>
2019-12-25 09:51:14 -0800

    Add DataBinding

--- app/src/main/java/com/example/simplequiz/QuizFragment.kt
+++ app/src/main/java/com/example/simplequiz/QuizFragment.kt
@@ -2,6 +2,7 @@ package com.example.simplequiz
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -48,8 +49,13 @@ class QuizFragment : Fragment() {
 
     private fun setQuestion(){
         currentQuestion = questions[questionIndex]
-        answers = currentQuestion.answerGroup
+        answers = ArrayList(currentQuestion.answerGroup)
         answers.shuffle()
+
+        Log.d("ANSWERGROUP", answers[0]+ " "+answers[1]+ " "+answers[2]+ " "+answers[3]+ " ")
+        Log.d("REALANSWER", currentQuestion.answerGroup[0])
+
+        binding.invalidateAll()
 
     }
 
@@ -66,7 +72,7 @@ class QuizFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_quiz_game,container,false)
 
         randomQuestion()
-
+        binding.quiz=this
         return binding.root
     }
 
