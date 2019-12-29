08fcea9d0b285a7376cb8c6186875f5b7ee57c12
pastoral <munir.hoque@gmail.com>
2019-12-29 09:51:08 -0800

    Navigation UP button added

--- app/src/main/java/com/example/simplequiz/MainActivity.kt
+++ app/src/main/java/com/example/simplequiz/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.simplequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = this.findNavController(R.id.hostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //return super.onSupportNavigateUp()
+        return navController.navigateUp()
     }
 }
